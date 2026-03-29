06cb0cfefaa2ad4b0be49adfa2e6795b3c84f1aa
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2026-03-29 21:26:21 +0600

    initialized project with resources

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    </>
+  )
+}
+
+export default App

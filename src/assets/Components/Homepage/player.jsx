29d698684bc40a1028f80f6969a6c6e9fc9ec8ff
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2026-03-31 20:15:38 +0600

    implemented toggle feature

--- src/assets/Components/Homepage/player.jsx
+++ src/assets/Components/Homepage/player.jsx
@@ -1,10 +1,31 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from '../Homepage/availablePlayers'
 
 const Player = ({playerPromise}) => {
     const players = use(playerPromise);
+    const [playerCatagory, setPlayerCatagory] = useState("selected");
+
     return (
-        <AvailablePlayers players={players}/>
+        <div className='max-w-330 mx-auto my-8'>
+            <div className='flex justify-between'>
+                {
+                    playerCatagory === "available" ? 
+                        (<h1 className='font-bold text-2xl'>Available Players</h1>) : 
+                        (<h1 className='font-bold text-2xl'>Selected Players</h1>)
+                }
+                <div className='flex'>
+                    <button onClick={() => setPlayerCatagory('available')} 
+                        className={`btn ${playerCatagory === 'available' ? "bg-yellow-300" : ""} rounded-r-none rounded-xl font-bold`}>
+                        Available
+                    </button>
+                    <button onClick={() => setPlayerCatagory('selected')} 
+                        className={`btn ${playerCatagory === 'selected' ? "bg-yellow-300" : ""} rounded-l-none rounded-xl font-bold`}>
+                        Selected()
+                    </button>
+                </div>
+            </div>
+            <AvailablePlayers players={players}/>
+        </div>
     );
 };
 
